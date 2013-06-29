91057071666edc3eabba1034cb0ea25671d5bc56
Filipe Ximenes <filipeximenes@gmail.com>
2013-06-29 19:00:54 -0300

    pegando dados base dos senadores

--- app/models/politico.rb
+++ app/models/politico.rb
@@ -1,5 +1,13 @@
 class Politico < ActiveRecord::Base
   attr_accessible :data_nascimento, :email, :foto, :nome, :sexo, :uf_nascimento, :midia_social, :contato
-  serialize :field, midia_social
-  serialize :field, contato
+  serialize :midia_social, JSON
+  serialize :contato, JSON
+
+  def self.clean_content att
+    if att
+      att.first
+    else
+      ''
+    end
+  end
 end
