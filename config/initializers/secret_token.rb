aba940bc02002c6720e73544ee72f527660d48f1
Andre Braga <abbraga@gmail.com>
2013-06-29 16:20:32 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoliticAPI::Application.config.secret_token = 'f93c002a02a140171702fe5e50973a9e00f8b7157348877f2dfda4e47e0c78f400b48ef284521e45d052e0652f83cf8d8d8fa45b982f341622eb361941c4e298'
